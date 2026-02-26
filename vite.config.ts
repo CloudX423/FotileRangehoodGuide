222a53b60ee17923b55989d1bd64baa806069566
CloudX423 <cloudxiawow@gmail.com>
2026-02-26 22:21:20 +1300

    Add GitHub Actions deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
+  base: isGitHubPages ? '/FotileRanghoodGuide/' : '/',
   server: {
     port: 5000,
     host: '0.0.0.0',
